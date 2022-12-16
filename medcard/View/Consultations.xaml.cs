217a5ee8ac111d896ff4604bae1e1af150204d05
rancerenly <rancerenly@gmail.com>
2022-12-16 22:04:11 +0700

    #4 view has been created

---
+++ medcard/View/Consultations.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace medcard.View
+{
+    public partial class Consultations : Page
+    {
+        public Consultations()
+        {
+            InitializeComponent();
+        }
+    }
+}
